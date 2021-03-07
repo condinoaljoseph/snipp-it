9f204ff3554e934fa7202d39cc99be89ae8028ca
Al Joseph <condinoaljoseph@gmail.com>
2021-03-07 21:29:37 +0800

    next auth provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from 'next-auth/client';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<ChakraProvider>
-			<Component {...pageProps} />
-		</ChakraProvider>
+		<Provider session={pageProps.session}>
+			<ChakraProvider>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</Provider>
 	);
 }
 
