a10f919b03c8a6b36e8bd805eecc1ab24f26ccfc
Al Joseph <condinoaljoseph@gmail.com>
2021-03-08 09:32:08 +0800

    added aside

---
+++ components/Aside.tsx
@@ -0,0 +1,11 @@
+import { Box, Avatar } from '@chakra-ui/react';
+
+const Aside: React.FC = () => {
+	return (
+		<Box>
+			<Avatar size="xs" name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
+		</Box>
+	);
+};
+
+export default Aside;
