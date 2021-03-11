ef8bd4b8b22447ccbb7d79b4b70212dc43c70268
Al Joseph <condinoaljoseph@gmail.com>
2021-03-11 19:21:53 +0800

    queried users

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import { GetStaticProps } from 'next';
 import prisma from '../lib/prisma';
 import Post, { PostProps } from '../components/Post';
+import { UserProps } from '../components/Aside';
 import Layout from '../components/Layout';
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -14,17 +15,25 @@ export const getStaticProps: GetStaticProps = async () => {
 		}
 	});
 
-	return { props: { feed } };
+	const users = await prisma.user.findMany({
+		take: 10,
+		select: { id: true, name: true, email: true, image: true }
+	});
+
+	return { props: { feed, users } };
 };
 
-export const Home: React.FC<{ feed: PostProps[] }> = ({ feed }) => {
+export const Home: React.FC<{ feed: PostProps[]; users: UserProps[] }> = ({
+	feed,
+	users
+}) => {
 	return (
 		<>
 			<Head>
 				<title>snipp-it</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<Layout>
+			<Layout users={users}>
 				{feed.map((post) => (
 					<Post key={post.id} post={post} />
 				))}
