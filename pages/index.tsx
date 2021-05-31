ed4ec6d5fbb008c78b5db962337b902e194613c1
Al Joseph <condinoaljoseph@gmail.com>
2021-05-31 23:13:26 +0800

    use swr to cache content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,14 @@
-import Head from 'next/head';
 import { GetStaticProps } from 'next';
-import prisma from '../lib/prisma';
-import Post, { PostProps } from '../components/Post';
-import { UserProps } from '../components/Aside';
-import Layout from '../components/Layout';
+import Head from 'next/head';
+import useSWR from 'swr';
+import fetcher from '@/lib/fetcher';
+import Post, { PostProps } from '@/components/Post';
+import { UserProps } from '@/components/Aside';
+import Layout from '@/components/Layout';
 
 export const getStaticProps: GetStaticProps = async () => {
-	const feed = await prisma.post.findMany({
-		where: { published: true },
-		include: {
-			author: {
-				select: { name: true, image: true }
-			}
-		}
-	});
-
-	const users = await prisma.user.findMany({
-		take: 10,
-		select: { id: true, name: true, email: true, image: true }
-	});
+	const data = await fetcher('http://localhost:3000/api/');
+	const { feed, users } = data;
 
 	return { props: { feed, users } };
 };
@@ -27,14 +17,16 @@ export const Home: React.FC<{ feed: PostProps[]; users: UserProps[] }> = ({
 	feed,
 	users
 }) => {
+	const { data } = useSWR('/api', fetcher, { initialData: { feed, users } });
+
 	return (
 		<>
 			<Head>
 				<title>snipp-it</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<Layout users={users}>
-				{feed.map((post) => (
+			<Layout users={data.users}>
+				{data.feed.map((post) => (
 					<Post key={post.id} post={post} />
 				))}
 			</Layout>
